ccb61b6e2cda852f8aa0339afe3bf10f63209cdf
Buddha_Shrestha <buddhashrestha.ioteng.977@gmail.com>
2025-10-25 15:54:03 +0545

    updating PyTorch_Neural_Networks_1.py file

--- PyTorch_Neural_Networks/PyTorch_Neural_Networks_1.py
+++ PyTorch_Neural_Networks/PyTorch_Neural_Networks_1.py
@@ -6,9 +6,14 @@ import torch
 import torch.nn as nn
 # importing matplot
 import matplotlib.pyplot as plt
-import matplotlib
+# import matplotlib
+
+# Displays the back-application used for graphing
+# print(f"Current backend: {matplotlib.get_backend()}") 
+
+# If default Agg download Tkinter and apply:
 # matplotlib.use('TkAgg')
-print(f"Current backend: {matplotlib.get_backend()}")
+
 # Creating Known parameter
 weight = 0.7
 bias = 0.3
@@ -56,9 +61,9 @@ def plot_prediction(train_data=X_train,
     if predictions is not None:
         # plot the predictions if there exist
         plt.scatter(test_data, predictions,
-                    color='#00ff00', label="Prediction")
+                    color='#aa0aaf', label="Prediction")
         # plt.savefig("Prediction.png")
-        print("Prediction figure been saved")
+        # print("Prediction figure been saved")
     # Show the legend
     plt.legend(prop={"size": 14})
     plt.title("Plot with Legend")
@@ -67,5 +72,47 @@ def plot_prediction(train_data=X_train,
 
 
 plot_prediction()
-print("Successfully Complete")
+# print("Successfully Complete")
 
+
+
+class LinearRegressionModel(nn.Module): # <-- almost everything in PyTorch inhits from nn.module
+    def __init__(self):
+        super().__init__()
+        self.weights = nn.Parameter(torch.randn(1,
+                                                requires_grad=True,
+                                                dtype=torch.float))
+        self.bias = nn.Parameter(torch.randn(
+            1,
+            requires_grad=True,
+            dtype=torch.float
+        ))
+    # Forward method to define the computation in the model
+    def forward(self,x:torch.Tensor) -> torch.Tensor :
+            return self.weights * x + self.bias # this is the linear regression formula
+        
+
+# Implementation of Random seed
+torch.manual_seed(42)
+
+# Create an instance of the model (this is a subclass of nn.module)
+model_0 = LinearRegressionModel()
+
+# Check out the parameters
+print(list(model_0.parameters()))
+
+# List named parameters
+print(model_0.state_dict())
+
+# Making prediction using `torch.inference_mode()`
+with torch.inference_mode():
+    y_preds = model_0(X_test)
+
+print(y_preds)
+
+plot_prediction(predictions=y_preds)
+
+# Setup a loss funtion 
+loss_fn = nn.L1Loss()
+
+print(loss_fn)
